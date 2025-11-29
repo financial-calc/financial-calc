b714cc22557abfcb4d5e1cb363425e8c13f13a06
vitorb4yousee <vitor.beltrao@4yousee.com.br>
2025-11-29 10:20:34 -0300

    release: update init files

---
+++ financial_calc/financial_math/__init__.py
@@ -0,0 +1,11 @@
+from financial_math import (
+    capitalization_regime,
+    uniform_payments_series,
+    investment_evaluation_methods,
+)
+
+__all__ = [
+    "capitalization_regime",
+    "uniform_payments_series",
+    "investment_evaluation_methods",
+]
